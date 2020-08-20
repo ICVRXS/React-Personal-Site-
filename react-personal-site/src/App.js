dfb44935fbaa965113e25dbd1982517a5e759096
unknown <calebhay@rocketmail.com>
2020-08-19 20:06:22 -0700

    Starting to build React app. Frameworking and planning in the works

---
+++ react-personal-site/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Router, Switch, Route} from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, world</h1>
+    </div>
+  );
+
+}
+
+export default App;
