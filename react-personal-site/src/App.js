f3efa8995a8207057cc9b40f6651dcf4965b9348
unknown <calebhay@rocketmail.com>
2020-08-21 19:52:55 -0700

    Projects now rendering dynamically. Proper styling still needed

--- react-personal-site/src/App.js
+++ react-personal-site/src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
+import ProjectCard from "./components/ProjectCard";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import projects from "./projects.json";
 
-const App = () => {
-  return (
-    <Router>
-      <About />
-      <Portfolio />
-      <Contact />
-    </Router>
-  );
+class App extends Component {
+  state = {
+    projects
+  };
 
+  render() {
+    return (
+      <Router>
+        <About />
+        {this.state.projects.map(project => (
+          <ProjectCard
+            id={project.id}
+            name={project.name}
+            image={project.image}
+            about={project.about}
+            repository={project.repository}
+            heroku={project.heroku}
+            />
+        ))}
+        <Contact />
+      </Router>
+    );
+
+  }
 }
 
 export default App;
---
+++ react-personal-site/src/components/ProjectCard/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Container, Row, Col, CardGroup, Card} from "react-bootstrap";
+
+function ProjectCard(props) {
+    return (
+        <Container>
+            <Row>
+                <Col size="md-6">
+                    <CardGroup>
+                        <Col size="md-4">
+                            <Card>
+                                <Card.Img src="https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png" variant="top" alt="Screenshot of app" />
+                                <Card.Body>
+                                    <h5 class="card-title">{props.name}</h5>
+                                    <p class="card-text">{props.about}</p>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small class="text-muted"><a href={props.repository} target="_blank">Repository</a></small>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default ProjectCard;
