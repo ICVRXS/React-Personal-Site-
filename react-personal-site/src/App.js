21698452ccb89d6d75f5c5e51756d8c3eba04e7e
unknown <calebhay@rocketmail.com>
2020-08-21 18:53:00 -0700

    Card page added, rendering as large card on main page with placeholder image.

--- react-personal-site/src/App.js
+++ react-personal-site/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <About />
+      <Portfolio />
       <Contact />
     </Router>
   );
