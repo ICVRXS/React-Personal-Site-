2f570c79a531486c451abec037e585a9a3798912
unknown <calebhay@rocketmail.com>
2020-08-24 19:51:12 -0700

    Routes set up, working. Starting work on content container for each page

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import ProjectCard from "./components/ProjectCard";
+import Nav from "./components/Nav";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import projects from "./projects.json";
+import {Jumbotron} from "react-bootstrap";
 
 class App extends Component {
   state = {
@@ -14,21 +16,55 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <About />
-        {this.state.projects.map(project => (
-          <ProjectCard
-            id={project.id}
-            name={project.name}
-            image={project.image}
-            about={project.about}
-            repository={project.repository}
-            heroku={project.heroku}
-            />
-        ))}
-        <Contact />
+        <Switch>
+          <Jumbotron>
+            <Nav />
+            <Route exact path={["/", "/about", "/home"]}>
+              <About />
+            </Route>
+            <Route exact path={["/portfolio"]}>
+              {this.state.projects.map(project => (
+                <ProjectCard
+                  id={project.id}
+                  name={project.name}
+                  image={project.image}
+                  about={project.about}
+                  repository={project.repository}
+                  heroku={project.heroku}
+                  />
+              ))}
+              </Route>
+              <Route exact path={["/contact"]}>
+                <Contact />
+              </Route>
+            </Jumbotron>
+        </Switch>
       </Router>
     );
   }
 }
 
 export default App;
+
+{/* <Router>
+<div>
+  <Switch>
+    <Route exact path={["/", "/about"]}>
+      <About />
+    </Route>
+    <Route exact path={["/portfolio"]}>
+      <ProjectCard
+          id={project.id}
+          name={project.name}
+          image={project.image}
+          about={project.about}
+          repository={project.repository}
+          heroku={project.heroku}
+      />
+    </Route>
+    <Route exact path={["/contact"]}>
+      <Contact />
+    </Route>
+  </Switch>
+</div>
+</Router> */}
--- client/src/pages/Portfolio/index.js
+++ client/src/pages/Portfolio/index.js
@@ -1,29 +1,8 @@
 import React from "react";
-import {Container, Row, Col, CardGroup, Card} from "react-bootstrap";
+import {ProjectCard, project} from "../../components/ProjectCard";
 
 function Portfolio() {
-    return (
-        <Container>
-            <Row>
-                <Col size="md-6">
-                    <CardGroup>
-                        <Col size="md-4">
-                            <Card>
-                                <Card.Img src="https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png" variant="top" alt="Screenshot of app" />
-                                <Card.Body>
-                                    <h5 class="card-title">Daily Planner</h5>
-                                    <p class="card-text">A daily planner which saves your data and colors each hour element based on the time of pageload.</p>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <small class="text-muted"><a href="https://github.com/ICVRXS/5.Daily_Planner" target="_blank">Repository</a></small>
-                                </Card.Footer>
-                            </Card>
-                        </Col>
-                    </CardGroup>
-                </Col>
-            </Row>
-        </Container>
-    );
+
 }
 
 export default Portfolio;
